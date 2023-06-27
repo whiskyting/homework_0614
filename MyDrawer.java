4d9fa5c713c21de0bb036dea513300e70f5ee031
whiskyting <136594015+whiskyting@users.noreply.github.com>
2023-06-27 16:39:28 +0800

    Add files via upload

---
+++ MyDrawer.java
@@ -0,0 +1,81 @@
+package tw.org.iii.classes;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import javax.swing.DebugGraphics;
+import javax.swing.JPanel;
+
+public class MyDrawer extends JPanel{
+	private LinkedList< LinkedList< HashMap<String, Integer>>> lines;
+	
+	
+	public MyDrawer() {
+		lines = new LinkedList<>();
+		setBackground(Color.green);
+		MyListener myListener = new MyListener();
+		addMouseListener(myListener);
+		addMouseMotionListener(myListener);
+	}
+	
+	private class MyListener extends MouseAdapter{
+		@Override
+		public void mousePressed(MouseEvent e) {
+			int x = e.getX(),y = e.getY();
+			HashMap<String, Integer> point =new HashMap<>();
+			point.put("x",x);point.put("y", y);
+			
+			LinkedList< HashMap<String, Integer>>line = new LinkedList<>();
+			line.add(point);
+			
+			lines.add(line);
+			repaint();
+		}
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			
+		}
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			int x = e.getX(),y = e.getY();
+			HashMap<String, Integer> point =new HashMap<>();
+			point.put("x",x);point.put("y", y);
+			
+			lines.getLast().add(point);
+			
+			repaint();
+		}
+	}
+	
+	
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g;
+		
+		g2d.setColor(Color.red);
+		g2d.setStroke(new BasicStroke(15));
+		
+		for(LinkedList< HashMap<String, Integer>> line:lines) {
+			for (int i=1;i<line.size();i++) {
+				HashMap<String, Integer> p0 = line.get(i-1);
+				HashMap<String, Integer> p1 = line.get(i);
+				g2d.drawLine(p0.get("x"), p0.get("y"),p1.get("x"),p1.get("y"));
+			}
+		}
+	}
+		public void clear() {
+			lines.clear();
+			repaint();
+		}
+
+		
+		
+	}
+
+
---
+++ MySign.java
@@ -0,0 +1,54 @@
+package tw.org.iii.twtor;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import tw.org.iii.classes.MyDrawer;
+
+public class MySign extends JFrame{
+	private MyDrawer myDrawer;
+	private JButton clear , undo , redo;
+	
+	
+	public MySign() {
+		super("簽名APP");
+		setLayout(new BorderLayout());
+		
+		myDrawer = new MyDrawer();
+		add(myDrawer,BorderLayout.CENTER);
+		
+		clear = new JButton("清除");
+		undo = new JButton("上一");
+		redo = new JButton("復原");
+		JPanel top = new JPanel(new FlowLayout());
+		top.add(clear);top.add(undo);top.add(redo);
+		
+		add(top, BorderLayout.NORTH);
+		clear.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myDrawer.clear();
+				
+			}
+		});
+		
+		
+		setSize(640,480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+	
+
+	public static void main(String[] args) {
+		new MySign();
+
+	}
+
+}
